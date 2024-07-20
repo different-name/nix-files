2c0aabb0ee5d60f6f938a02c6b3207bc9f6d28aa
Different <github.shrubs231@passmail.net>
2024-07-20 22:25:56 +1000

    Basic waybar config

--- home/global/programs/wayland/waybar.nix
+++ home/global/programs/wayland/waybar.nix
@@ -1,6 +1,62 @@
-{
+{config, ...}: let
+  hyprlandCfg = config.wayland.windowManager.hyprland;
+  margin = hyprlandCfg.settings.general.gaps_out;
+  marginStr = builtins.toString margin;
+in {
   programs.waybar = {
     enable = true;
-    catppuccin.enable = true;
+
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+        height = 30;
+        modules-left = [
+          "hyprland/workspaces"
+        ];
+        modules-center = [
+        ];
+        modules-right = [
+          "privacy"
+          "systemd-failed-units"
+          "mpris"
+          "bluetooth"
+          "tray"
+          "clock"
+        ];
+        # margin = "${marginStr} ${marginStr} 0 ${marginStr}";
+        spacing = margin;
+        clock = {
+          format-alt = "{:%a, %d. %b  %H:%M}";
+        };
+
+        # modules-left
+
+        # modules-center
+
+        # modules-right
+        mpris = {
+          format = "{dynamic}";
+          format-paused = "<i>{dynamic}</i>";
+          dynamic-order = [
+            "title"
+            "artist"
+          ];
+        };
+        tray = {
+          spacing = margin;
+        };
+        clock = {
+          format = "{:%I:%M %p}";
+          on-click = "";
+        };
+      };
+    };
+
+    # style = ''
+    #   window#mainBar {
+    #     padding: 5px;
+    #   }
+    # '';
   };
 }
