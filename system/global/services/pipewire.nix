849562a82f98ba48aa505df34446ed0bb8a2fbe9
Different <github.shrubs231@passmail.net>
2024-07-23 17:09:43 +1000

    #43

--- system/global/services/pipewire.nix
+++ system/global/services/pipewire.nix
@@ -5,7 +5,26 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
-    wireplumber.enable = true;
+    wireplumber = {
+      enable = true;
+      # https://askubuntu.com/a/1510696
+      # Disable WebRTC in chromium from writing to input volume
+      # Weird feature, allows websites with microphone access to set your microphone volume
+      extraConfig = {
+        access.rules = [
+          {
+            matches = [
+              {
+                "client.name" = "~(Chromium|Brave|electron)( input)?";
+              }
+            ];
+            actions = {
+              quirks = ["block-source-volume"];
+            };
+          }
+        ];
+      };
+    };
   };
 
   hardware.pulseaudio.enable = lib.mkForce false;
