ea8b539bedad4ba2e55c5f5a21d376c9b769c40f
Different <github.shrubs231@passmail.net>
2024-07-28 20:51:16 +1000

    Use tarball for ALVR

--- pkgs/alvr/default.nix
+++ pkgs/alvr/default.nix
@@ -1,9 +1,8 @@
 {
   stdenv,
+  autoPatchelfHook,
   lib,
-  fetchFromGitHub,
-  fetchzip,
-  rustPlatform,
+  fetchurl,
   pkg-config,
   jack2,
   alsaLib,
@@ -19,78 +18,32 @@
   vulkan-validation-layers,
   libunwind,
   xorg,
-  git,
-  nasm,
-  # libgcc,
-  # libGL,
-  # libxkbcommon,
-  # wayland,
-  # binutils-unwrapped,
-  # glib,
-  # cairo,
-  # pango,
-  # atk,
-  # gdk-pixbuf,
-  # gtk3,
-  # python3,
-  # libxkbcommon
+  wayland,
+  libGL,
+  wayland-protocols,
 }: let
   pname = "alvr";
   version = "20.9.1";
 
-  src = fetchFromGitHub {
-    owner = "alvr-org";
-    repo = "ALVR";
-    rev = "70bba37";
-    hash = "sha256-ettyLuMf6/0ihb6skq2r+u/KMgCmeomTySDF20Arbac=";
-    fetchSubmodules = true;
-    leaveDotGit = true;
-  };
-
-  x264-src = fetchzip {
-    url = "https://code.videolan.org/videolan/x264/-/archive/c196240409e4d7c01b47448d93b1f9683aaa7cf7/x264-c196240409e4d7c01b47448d93b1f9683aaa7cf7.tar.bz2";
-    hash = "sha256-5v4svrf+ZKo/KCvMtnkvHKv1MVJbXDNSjN49TTpsuAo=";
-  };
-
-  ffmpeg-n6-src = fetchzip {
-    url = "https://codeload.github.com/FFmpeg/FFmpeg/zip/n6.0";
-    hash = "sha256-RVbgsafIbeUUNXmUbDQ03ZN42oaUo0njqROo7KOQgv0=";
-    extension = ".zip";
-  };
-
-  ffmpeg-n12-src = fetchzip {
-    url = "https://github.com/FFmpeg/nv-codec-headers/archive/refs/tags/n12.1.14.0.zip";
-    hash = "sha256-WJYuFmMGSW+B32LwE7oXv/IeTln6TNEeXSkquHh85Go=";
+  src = fetchurl {
+    url = "https://github.com/alvr-org/ALVR/releases/download/v${version}/alvr_launcher_linux.tar.gz";
+    hash = "sha256-c2IBs/tS1Cvf20Pg89JO2jbqNF5F8KPsRmZS0/hnDio=";
   };
 in
-  rustPlatform.buildRustPackage rec {
+  stdenv.mkDerivation rec {
     inherit pname version src;
 
-    cargoLock = {
-      lockFile = "${src}/Cargo.lock";
-      outputHashes = {
-        "openxr-0.17.1" = "sha256-fG/JEqQQwKP5aerANAt5OeYYDZxcvUKCCaVdWRqHBPU=";
-        "settings-schema-0.2.0" = "sha256-luEdAKDTq76dMeo5kA+QDTHpRMFUg3n0qvyQ7DkId0k=";
-      };
-    };
-
     nativeBuildInputs = [
-      pkg-config
-      clang
-      git
-      nasm
+      autoPatchelfHook
     ];
 
     buildInputs = [
+      pkg-config
       jack2
       alsaLib
       openssl
       pipewire
       libva
-      (ffmpeg-full.overrideAttrs (oldAttrs: {
-        withUnfree = true;
-        withSamba = null;
-      }))
       vulkan-tools-lunarg
       vulkan-headers
       vulkan-loader
@@ -98,34 +51,22 @@ in
       libunwind
       xorg.libX11
       xorg.libXrandr
-
-      # binutils-unwrapped
-      # glib
-      # cairo
-      # pango
-      # atk
-      # gdk-pixbuf
-      # gtk3
-      # python3 # for the xcb crate
-      # libxkbcommon
+      wayland
+      libGL
+      wayland-protocols
     ];
 
-    patches = [
-      ./download.patch
-    ];
+    sourceRoot = "alvr_launcher_linux";
 
-    LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
+    installPhase = ''
+      runHook preInstall
+      install -m755 -D "ALVR Launcher" "$out/bin/${pname}"
+      runHook postInstall
+    '';
 
-    buildPhase = ''
-      mkdir -p /build/download
-      cp -r ${x264-src} /build/download/x264-c196240409e4d7c01b47448d93b1f9683aaa7cf7
-      cp -r ${ffmpeg-n6-src} /build/download/FFmpeg-n6.0
-      cp -r ${ffmpeg-n12-src} /build/download/nv-codec-headers-n12.1.14.0
-      chmod -R u+w /build/download/
-      patchShebangs --build /build/download/x264-c196240409e4d7c01b47448d93b1f9683aaa7cf7/configure
-
-      cargo xtask prepare-deps --platform linux
-      cargo xtask build-streamer --release
+    postFixup = ''
+      patchelf $out/bin/${pname} \
+        --add-rpath ${lib.makeLibraryPath buildInputs}
     '';
 
     meta = with lib; {
