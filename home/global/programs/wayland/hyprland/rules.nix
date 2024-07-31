9b58e57709631b2012c09370f17c7154ef70bbe3
Different <github.shrubs231@passmail.net>
2024-08-01 01:23:04 +1000

    Window rules typo fix

--- home/global/programs/wayland/hyprland/rules.nix
+++ home/global/programs/wayland/hyprland/rules.nix
@@ -1,34 +1,34 @@
 {lib, ...}: {
   wayland.windowManager.hyprland.settings = {
     windowrulev2 = [
-        # galculate
-        "float, class:^(qalculate-gtk)$"
-        "size 850 575, class:^(qalculate-gtk)$"
+      # galculate
+      "float, class:^(qalculate-gtk)$"
+      "size 850 575, class:^(qalculate-gtk)$"
 
-        # brave PiP
-        "float, title:^(Picture-in-picture)$"
-        "pin, title:^(Picture-in-picture)$"
-        "size 480 270, title:^(Picture-in-picture)$"
+      # brave PiP
+      "float, title:^(Picture-in-picture)$"
+      "pin, title:^(Picture-in-picture)$"
+      "size 480 270, title:^(Picture-in-picture)$"
 
-        # gtk popups
-        "float, class:^(xdg-desktop-portal-gtk)$"
+      # gtk popups
+      "float, class:^(xdg-desktop-portal-gtk)$"
 
-        # pavucontrol
-        "float, class:^(pavucontrol)$"
-        "size 1000 750, class:^(pavucontrol)$"
+      # pavucontrol
+      "float, class:^(pavucontrol)$"
+      "size 1000 750, class:^(pavucontrol)$"
 
-        # hyprland share picker
-        "float, title:^(MainPicker)$"
+      # hyprland share picker
+      "float, title:^(MainPicker)$"
 
-        # steam games
-        "suppressevent maximize, class:^(steam_app_[0-9]*)$"
-        "suppressevent fullscreen, class:^(steam_app_[0-9]*)$"
-        "fullscreen, class:%(steam_app_[0-9]*)$"
-        "workspace 2, class:^(steam_app_[0-9]*)$"
+      # steam games
+      "suppressevent maximize, class:^(steam_app_[0-9]*)$"
+      "suppressevent fullscreen, class:^(steam_app_[0-9]*)$"
+      "fullscreen, class:^(steam_app_[0-9]*)$"
+      "workspace 2, class:^(steam_app_[0-9]*)$"
 
-        # fix for borderlands mouse not working
-        # possibly related to https://github.com/hyprwm/Hyprland/issues/6543
-        "stayfocused, class:^(steam_app_49520)$"
-      ];
+      # fix for borderlands mouse not working
+      # possibly related to https://github.com/hyprwm/Hyprland/issues/6543
+      "stayfocused, class:^(steam_app_49520)$"
+    ];
   };
 }
