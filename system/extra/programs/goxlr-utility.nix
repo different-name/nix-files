c3c122ebfc1e6beeac195ae8d3c0decf1b6f52a2
Different <github.shrubs231@passmail.net>
2024-08-01 01:22:12 +1000

    goxlr-utility desktop entry

--- system/extra/programs/goxlr-utility.nix
+++ system/extra/programs/goxlr-utility.nix
@@ -1,3 +1,9 @@
-{
-  services.goxlr-utility.enable = true;
+{pkgs, ...}: {
+  services.goxlr-utility = {
+    enable = true;
+    # workaround for https://github.com/NixOS/nixpkgs/issues/331319
+    package = pkgs.goxlr-utility.overrideAttrs (oldAttrs: {
+      postInstall = builtins.replaceStrings ["--replace goxlr-launcher goxlr-daemon"] [""] oldAttrs.postInstall;
+    });
+  };
 }
