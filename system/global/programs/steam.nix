24d95169cd0a69910c4555eb78c0added53e0e61
Different <github.shrubs231@passmail.net>
2024-07-31 16:08:23 +1000

    Remove gamescope packages

--- system/global/programs/steam.nix
+++ system/global/programs/steam.nix
@@ -24,23 +24,6 @@ in {
         };
       }))
     ];
-
-    # fix gamescope inside of steam
-    package = pkgs.steam.override {
-      extraPkgs = pkgs:
-        with pkgs; [
-          keyutils
-          libkrb5
-          libpng
-          libpulseaudio
-          libvorbis
-          stdenv.cc.cc.lib
-          xorg.libXcursor
-          xorg.libXi
-          xorg.libXinerama
-          xorg.libXScrnSaver
-        ];
-    };
   };
 
   systemd.tmpfiles.rules = [
