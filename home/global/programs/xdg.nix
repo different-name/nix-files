840c4581e19ad239a51fcae12c3870c11907ff40
Different <github.shrubs231@passmail.net>
2024-08-12 22:20:47 +1000

    Fixes #47

--- home/global/programs/xdg.nix
+++ home/global/programs/xdg.nix
@@ -4,10 +4,33 @@
 
     mime.enable = true;
 
+    desktopEntries = {
+      imv = {
+        name = "imv";
+        comment = "Image Viewer";
+        exec = "imv %U";
+        icon = "imv";
+        terminal = false;
+        type = "Application";
+        categories = ["Graphics"];
+      };
+    };
+
     mimeApps = {
       enable = true;
       defaultApplications = {
+        # directories
         "inode/directory" = "thunar.desktop";
+
+        # images
+        "image/png" = "imv.desktop";
+        "image/jpeg" = "imv.desktop";
+        "image/gif" = "imv.desktop";
+        "image/bmp" = "imv.desktop";
+        "image/tiff" = "imv.desktop";
+        "image/x-icon" = "imv.desktop";
+
+        # unity login
         "x-scheme-handler/unityhub" = "unityhub.desktop";
       };
     };
