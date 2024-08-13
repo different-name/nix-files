96e5a7069964214d2d2ef0643de6e020271baa7a
Different <github.shrubs231@passmail.net>
2024-08-14 06:57:49 +1000

    Remove goxlr workaround, fixed upstream

--- system/extra/programs/goxlr-utility.nix
+++ system/extra/programs/goxlr-utility.nix
@@ -1,9 +1,3 @@
-{pkgs, ...}: {
-  services.goxlr-utility = {
-    enable = true;
-    # workaround for https://github.com/NixOS/nixpkgs/issues/331319
-    package = pkgs.goxlr-utility.overrideAttrs (oldAttrs: {
-      postInstall = builtins.replaceStrings ["--replace goxlr-launcher goxlr-daemon"] [""] oldAttrs.postInstall;
-    });
-  };
+{
+  services.goxlr-utility.enable = true;
 }
