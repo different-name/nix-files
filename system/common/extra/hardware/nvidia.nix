742336236869839a773e1fc068d6a8ee36f4339d
Different <github.shrubs231@passmail.net>
2024-08-19 05:30:49 +1000

    Switch to beta 560 drivers

--- system/common/extra/hardware/nvidia.nix
+++ system/common/extra/hardware/nvidia.nix
@@ -1,4 +1,4 @@
-{
+{config, lib, ...}: {
   # load nvidia driver for xorg and wayland
   services.xserver.videoDrivers = ["nvidia"];
 
@@ -11,5 +11,14 @@
     open = false;
 
     nvidiaSettings = false;
+
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "560.31.02";
+      sha256_64bit = "sha256-0cwgejoFsefl2M6jdWZC+CKc58CqOXDjSi4saVPNKY0=";
+      sha256_aarch64 = lib.fakeSha256;
+      openSha256 = lib.fakeSha256;
+      settingsSha256 = lib.fakeSha256;
+      persistencedSha256 = lib.fakeSha256;
+    };
   };
 }
