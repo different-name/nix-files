4e9e83462267816455e5f3d16e9cfaf33a0cff7d
Different <github.shrubs231@passmail.net>
2024-08-21 03:41:35 +1000

    Organise flake inputs + update

--- flake.nix
+++ flake.nix
@@ -2,14 +2,14 @@
   description = "Different's nix-files";
 
   inputs = {
-    # nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
     # secrets management
     agenix = {
       url = "github:ryantm/agenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
+      inputs = {
+        home-manager.follows = "home-manager";
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+      };
     };
 
     # color theme
@@ -27,14 +27,54 @@
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
 
+    # hardware configurations
+    hardware.url = "github:nixos/nixos-hardware";
+
     # manages user environment
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # hardware configurations
-    hardware.url = "github:nixos/nixos-hardware";
+    # hyprwm
+    hyprland = {
+      url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+      };
+    };
+    hyprland-contrib = {
+      url = "github:hyprwm/contrib";
+      inputs.nixpkgs.follows = "hyprland/nixpkgs";
+    };
+    hyprlock = {
+      url = "github:hyprwm/hyprlock";
+      inputs = {
+        hyprlang.follows = "hyprland/hyprlang";
+        hyprutils.follows = "hyprland/hyprutils";
+        nixpkgs.follows = "hyprland/nixpkgs";
+        systems.follows = "hyprland/systems";
+      };
+    };
+    hyprpaper = {
+      url = "github:hyprwm/hyprpaper";
+      inputs = {
+        hyprlang.follows = "hyprland/hyprlang";
+        hyprutils.follows = "hyprland/hyprutils";
+        hyprwayland-scanner.follows = "hyprland/hyprwayland-scanner";
+        nixpkgs.follows = "hyprland/nixpkgs";
+        systems.follows = "hyprland/systems";
+      };
+    };
+    hyprpicker = {
+      url = "github:hyprwm/hyprpicker";
+      inputs = {
+        hyprutils.follows = "hyprland/hyprutils";
+        nixpkgs.follows = "hyprland/nixpkgs";
+        systems.follows = "hyprland/systems";
+      };
+    };
 
     # manage persistent state
     impermanence.url = "github:nix-community/impermanence";
@@ -49,45 +89,14 @@
     pre-commit-hooks-nix = {
       url = "github:cachix/pre-commit-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs-stable.follows = "nixpkgs";
     };
 
-    # hyprwm
-    hyprland = {
-      url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    # hypridle = {
-    #   url = "github:hyprwm/hypridle";
-    #   inputs.hyprlang.follows = "hyprland/hyprlang";
-    #   inputs.nixpkgs.follows = "hyprland/nixpkgs";
-    #   inputs.systems.follows = "hyprland/systems";
-    # };
-
-    hyprland-contrib = {
-      url = "github:hyprwm/contrib";
-      inputs.nixpkgs.follows = "hyprland/nixpkgs";
-    };
-
-    hyprlock = {
-      url = "github:hyprwm/hyprlock";
-      inputs.hyprlang.follows = "hyprland/hyprlang";
-      inputs.nixpkgs.follows = "hyprland/nixpkgs";
-      inputs.systems.follows = "hyprland/systems";
-    };
-
-    hyprpaper = {
-      url = "github:hyprwm/hyprpaper";
-      inputs.hyprlang.follows = "hyprland/hyprlang";
-      inputs.nixpkgs.follows = "hyprland/nixpkgs";
-      inputs.systems.follows = "hyprland/systems";
-    };
-
-    hyprpicker = {
-      url = "github:hyprwm/hyprpicker";
-      inputs.nixpkgs.follows = "hyprland/nixpkgs";
-      inputs.systems.follows = "hyprland/systems";
-    };
+    # list of systems
+    systems.url = "github:nix-systems/default-linux";
   };
 
   outputs = inputs:
