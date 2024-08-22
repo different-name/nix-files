32b623782374330d1138f1f7cb7f747a2b466e6d
Different <github.shrubs231@passmail.net>
2024-08-22 12:36:34 +1000

    Remove stray comment

--- modules/default.nix
+++ modules/default.nix
@@ -1,7 +1,5 @@
 {
   flake.nixosModules = {
-    # Module that contains nix expressions specific to this
-    # config / my use case that I don't have plans to upstream
     ephemeral-tools = import ./ephemeral-tools.nix;
   };
 }
