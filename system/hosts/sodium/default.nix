4bd5404ef6d7d6bbdc838d33ffcd81b6c4f576d3
Different <github.shrubs231@passmail.net>
2024-08-25 17:39:33 +1000

    Sodium local backup script

--- system/hosts/sodium/default.nix
+++ system/hosts/sodium/default.nix
@@ -1,6 +1,7 @@
 {
   inputs,
   self,
+  pkgs,
   ...
 }: {
   imports = [
@@ -43,6 +44,25 @@
     STEAM_FORCE_DESKTOPUI_SCALING = "1.5";
     GDK_SCALE = "2";
   };
+
+  # script used to backup to external hdd
+  environment.systemPackages = [
+    (pkgs.writeShellApplication {
+      name = "runbackup";
+      text = let
+        source = "/persist";
+        partitionId = "wwn-0x50014ee2145675c9-part1";
+        partitionPath = "/dev/disk/by-id/${partitionId}";
+        mountpoint = "/tmp/${partitionId}";
+      in ''
+        mkdir -p "${mountpoint}"
+        mount -t ext4 "${partitionPath}" "${mountpoint}"
+        rsync "${source}" "${mountpoint}" -avh --delete --progress --exclude /persist/home/different/.cache
+        udisksctl unmount -b "${partitionPath}"
+        udisksctl power-off -b "${partitionPath}"
+      '';
+    })
+  ];
 
   # https://wiki.nixos.org/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "24.05";
