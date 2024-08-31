e07f0c719210e684330892b373b6cf8012f4c2a9
Different <github.shrubs231@passmail.net>
2024-08-31 12:52:51 +1000

    Update

--- system/common/desktop/programs/steam.nix
+++ system/common/desktop/programs/steam.nix
@@ -11,9 +11,9 @@
       # proton ge with rtsp patch, for vrchat video players
       (proton-ge-bin.overrideAttrs (finalAttrs: {
         pname = "proton-ge-rtsp-bin";
-        version = "GE-Proton9-10-rtsp14";
+        version = "GE-Proton9-10-rtsp15";
         src = pkgs.fetchzip {
-          url = "https://github.com/SpookySkeletons/proton-ge-rtsp/releases/download/GE-Proton9-10-rtsp14/GE-Proton9-10-rtsp14.tar.gz";
+          url = "https://github.com/SpookySkeletons/proton-ge-rtsp/releases/download/GE-Proton9-10-rtsp15/GE-Proton9-10-rtsp15.tar.gz";
           hash = "sha256-4Ii2vnnQcoAxjYrDl0gDhllVqfUOsZxkEMIsxSwgdN8=";
         };
       }))
