c315c2185bd5fbceb12b39725808c91b2ed65592
Different <github.shrubs231@passmail.net>
2024-09-04 12:50:23 +1000

    Use flake input for hyprland xdg portal

--- system/common/desktop/programs/hyprland.nix
+++ system/common/desktop/programs/hyprland.nix
@@ -6,6 +6,7 @@
   programs.hyprland = {
     enable = true;
     package = inputs.hyprland.packages."${pkgs.system}".hyprland;
+    portalPackage = inputs.hyprland.packages."${pkgs.system}".xdg-desktop-portal-hyprland;
   };
 
   # hint electron apps to use wayland
