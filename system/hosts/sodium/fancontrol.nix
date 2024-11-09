15280a70d3339f097b60eb465d8187290246784b
Different <hello@different-name.dev>
2024-11-09 13:32:41 +1000

    Add additional fan to fan control

--- system/hosts/sodium/fancontrol.nix
+++ system/hosts/sodium/fancontrol.nix
@@ -40,13 +40,13 @@
       INTERVAL=5
       DEVPATH=hwmon2=devices/platform/nct6775.656 hwmon3=devices/pci0000:00/0000:00:18.3
       DEVNAME=hwmon2=nct6798 hwmon3=k10temp
-      FCTEMPS=hwmon2/pwm1=/run/nvidia-temp hwmon2/pwm2=hwmon3/temp1_input hwmon2/pwm3=hwmon3/temp1_input hwmon2/pwm4=hwmon3/temp1_input hwmon2/pwm5=/run/nvidia-temp
-      FCFANS=hwmon2/pwm1=hwmon2/fan1_input hwmon2/pwm2=hwmon2/fan2_input hwmon2/pwm3=hwmon2/fan3_input hwmon2/pwm4=hwmon2/fan4_input hwmon2/pwm5=hwmon2/fan5_input
-      MINTEMP=hwmon2/pwm1=${GPU.minTemp} hwmon2/pwm2=${CPU.minTemp} hwmon2/pwm3=${CPU.minTemp} hwmon2/pwm4=${caseFan.minTemp} hwmon2/pwm5=${GPU.minTemp}
-      MAXTEMP=hwmon2/pwm1=${GPU.maxTemp} hwmon2/pwm2=${CPU.maxTemp} hwmon2/pwm3=${CPU.maxTemp} hwmon2/pwm4=${caseFan.maxTemp} hwmon2/pwm5=${GPU.maxTemp}
-      MINSTART=hwmon2/pwm1=${minStart} hwmon2/pwm2=${minStart} hwmon2/pwm3=${minStart} hwmon2/pwm4=${minStart} hwmon2/pwm5=${minStart}
-      MINSTOP=hwmon2/pwm1=${minStop} hwmon2/pwm2=${minStop} hwmon2/pwm3=${minStop} hwmon2/pwm4=${minStop} hwmon2/pwm5=${minStop}
-      MAXPWM=hwmon2/pwm1=${GPU.maxPWM} hwmon2/pwm2=${CPU.maxPWM} hwmon2/pwm3=${CPU.maxPWM} hwmon2/pwm4=${caseFan.maxPWM} hwmon2/pwm5=${GPU.maxPWM}
+      FCTEMPS=hwmon2/pwm1=/run/nvidia-temp hwmon2/pwm2=hwmon3/temp1_input hwmon2/pwm3=hwmon3/temp1_input hwmon2/pwm4=hwmon3/temp1_input hwmon2/pwm5=/run/nvidia-temp hwmon2/pwm7=/run/nvidia-temp
+      FCFANS=hwmon2/pwm1=hwmon2/fan1_input hwmon2/pwm2=hwmon2/fan2_input hwmon2/pwm3=hwmon2/fan3_input hwmon2/pwm4=hwmon2/fan4_input hwmon2/pwm5=hwmon2/fan5_input hwmon2/pwm7=hwmon2/fan7_input
+      MINTEMP=hwmon2/pwm1=${GPU.minTemp} hwmon2/pwm2=${CPU.minTemp} hwmon2/pwm3=${CPU.minTemp} hwmon2/pwm4=${caseFan.minTemp} hwmon2/pwm5=${GPU.minTemp} hwmon2/pwm7=${GPU.minTemp}
+      MAXTEMP=hwmon2/pwm1=${GPU.maxTemp} hwmon2/pwm2=${CPU.maxTemp} hwmon2/pwm3=${CPU.maxTemp} hwmon2/pwm4=${caseFan.maxTemp} hwmon2/pwm5=${GPU.maxTemp} hwmon2/pwm7=${GPU.maxTemp}
+      MINSTART=hwmon2/pwm1=${minStart} hwmon2/pwm2=${minStart} hwmon2/pwm3=${minStart} hwmon2/pwm4=${minStart} hwmon2/pwm5=${minStart} hwmon2/pwm7=${minStart}
+      MINSTOP=hwmon2/pwm1=${minStop} hwmon2/pwm2=${minStop} hwmon2/pwm3=${minStop} hwmon2/pwm4=${minStop} hwmon2/pwm5=${minStop} hwmon2/pwm7=${minStop}
+      MAXPWM=hwmon2/pwm1=${GPU.maxPWM} hwmon2/pwm2=${CPU.maxPWM} hwmon2/pwm3=${CPU.maxPWM} hwmon2/pwm4=${caseFan.maxPWM} hwmon2/pwm5=${GPU.maxPWM} hwmon2/pwm7=${GPU.maxPWM}
     '';
   };
 
