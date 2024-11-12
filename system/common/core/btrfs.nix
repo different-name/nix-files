8385246ab55d13a0dd508e8ce1229d9792a6d922
Different <hello@different-name.dev>
2024-11-12 12:12:51 +1000

    Btrfs root subvolume switcher

--- system/common/core/btrfs.nix
+++ system/common/core/btrfs.nix
@@ -1,45 +1,54 @@
 {
   lib,
   config,
-  # pkgs,
   ...
 }: {
   options.nix-files.core.btrfs.enable = lib.mkEnableOption "btrfs config";
 
   config = lib.mkIf config.nix-files.core.btrfs.enable {
-    # boot.initrd.systemd.services.root-subvol-switch = {
-    #   description = "switch root subvolume";
-    #   serviceConfig = {
-    #     Type = "oneshot";
-    #     RemainAfterExit = true;
-    #   };
-    #   requires = ["dev-mapper-foo.device"];
-    #   after = ["dev-mapper-foo.device"];
-    #   wantedBy = ["initrd.target"];
-    #   path = with pkgs; [btrfs-progs];
-    #   script = ''
-    #     waitDevice ${config.fileSystems."/".device}
+    boot.initrd.systemd = {
+      enable = true;
+      services.root-subvol-switch = {
+        description = "Switch btrfs root subvolume";
+        wantedBy = [
+          "initrd.target"
+        ];
+        after = [
+          "systemd-cryptsetup@rootfs.service"
+        ];
+        requires = [
+          "systemd-cryptsetup@rootfs.service"
+        ];
+        before = [
+          "sysroot.mount"
+        ];
+        # requires = ["initrd-root-device.target"];
+        # after = ["local-fs-pre.target" "initrd-root-device.target"];
+        # requiredBy = ["initrd-root-fs.target"];
+        # before = ["sysroot.mount"];
+        unitConfig.DefaultDependencies = "no";
+        serviceConfig.Type = "oneshot";
+        script = ''
+          mkdir -p /btrfs
+          mount -t btrfs -o subvol=/ ${config.fileSystems."/".device} /btrfs
 
-    #     mkdir -p /btrfs
-    #     mount ${config.fileSystems."/".device} /btrfs
+          if [[ -e /btrfs/root/current ]]; then
+            timestamp=$(date --date="@$(stat -c %Y /btrfs/root)" "+%Y-%m-%-d_%H:%M:%S")
+            mv /btrfs/root/current "/btrfs/root/$timestamp"
+          fi
 
-    #     if [ -e /btrfs/root/current ]; then
-    #       timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-    #       mv /btrfs/root/current "/btrfs/root/$timestamp"
-    #     fi
+          btrfs subvolume create /btrfs/root/current
 
-    #     btrfs subvolume create /mnt/root/current
-
-    #     umount /btrfs
-    #     rmdir /btrfs
-    #   '';
-    # };
+          umount /btrfs
+        '';
+      };
+    };
 
     # systemd.services.root-subvol-cleanup = let
     #   keepAtLeast = 5;
     #   cutoffDate = "30 days ago";
     # in {
-    #   description = "root subvolume cleaner";
+    #   description = "Btrfs root subvolume cleaner";
     #   startAt = "daily";
     #   script = ''
 
