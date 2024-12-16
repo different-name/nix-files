75cd60ab0496bc348adcc9bcf7d2fbd6835c0985
Different <hello@different-name.dev>
2024-12-16 11:05:58 +1000

    Home/Potassium: Battery level binding

--- home/users/different/hosts/potassium.nix
+++ home/users/different/hosts/potassium.nix
@@ -14,6 +14,13 @@
     graphical.meta.nvidia.enable = true;
   };
 
+  ### host specific
+
+  # display battery as notification
+  wayland.windowManager.hyprland.settings.bind = [
+    ''$mod, B, exec, cat /sys/class/power_supply/BAT0/capacity | xargs -I {} notify-send -t 5000 "Battery: {}%"''
+  ];
+
   ### required config
 
   # https://wiki.nixos.org/wiki/FAQ/When_do_I_update_stateVersion
