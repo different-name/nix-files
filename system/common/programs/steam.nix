85bc6a18b38a98a2da8738890b8a0f28c3e0eb34
Different <hello@different-name.dev>
2025-02-08 19:13:52 +1000

    System/Steam: Update from GE-Proton9-22-rtsp17 to GE-Proton9-22-rtsp17-1

--- system/common/programs/steam.nix
+++ system/common/programs/steam.nix
@@ -15,13 +15,13 @@
 
         # proton ge with rtsp patch, for vrchat video players
         (proton-ge-bin.overrideAttrs (finalAttrs: let
-          version = "GE-Proton9-22-rtsp17";
+          version = "GE-Proton9-22-rtsp17-1";
         in {
           pname = "proton-ge-rtsp-bin";
           inherit version;
           src = pkgs.fetchzip {
             url = "https://github.com/SpookySkeletons/proton-ge-rtsp/releases/download/${version}/${version}.tar.gz";
-            hash = "sha256-1zj0y7E9JWrnPC9HllFXos33rsdAt3q+NamoxNTmHHM=";
+            hash = "sha256-GeExWNW0J3Nfq5rcBGiG2BNEmBg0s6bavF68QqJfuX8=";
           };
         }))
       ];
