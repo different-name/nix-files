2d1ebf45e5ebc01d1ffd215b0bace3e1af3f6f9e
Different <hello@different-name.dev>
2025-03-13 14:46:04 +1000

    Home/Hyprland: Disable allow screen capture token by default

--- home/common/graphical/wayland/hyprland/settings.nix
+++ home/common/graphical/wayland/hyprland/settings.nix
@@ -121,14 +121,5 @@ in {
 
       debug.disable_logs = false;
     };
-
-    # TODO find or implement a way to do this through home-manager module
-    xdg.configFile."hypr/xdph.conf" = {
-      text = ''
-        screencopy {
-          allow_token_by_default = true
-        }
-      '';
-    };
   };
 }
