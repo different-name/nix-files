d45332035cb45fa1ca66a4442d4ed76d12b60ea4
Different <hello@different-name.dev>
2025-05-06 20:02:52 +1000

    Revert "Home/Hyprland: Disable application not responding dialog"
    This reverts commit a4826d1576f24acbe9ac7bdcf8bfae1ee5dc1449.

--- home/common/graphical/wayland/hyprland/settings.nix
+++ home/common/graphical/wayland/hyprland/settings.nix
@@ -86,7 +86,6 @@ in {
         disable_autoreload = true; # disable auto polling for config file changes
         disable_hyprland_logo = true;
         disable_splash_rendering = true;
-        enable_anr_dialog = false;
       };
 
       render.direct_scanout = false;
