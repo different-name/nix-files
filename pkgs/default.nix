a1e6ea0592300d326571d811e84e78915bd15f2d
Different <hello@different-name.dev>
2025-05-14 13:22:47 +1000

    [WIP] pkgs: implement nvfetcher wivrn-solarxr still does not build

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,21 @@
 {
   perSystem = {pkgs, ...}: {
-    packages = {
-      wivrn-solarxr = pkgs.callPackage ./wivrn-solarxr.nix {};
-    };
+    packages = let
+      nvSources = import ./_sources/generated.nix {
+        inherit (pkgs) fetchgit fetchurl fetchFromGitHub dockerTools;
+      };
+
+      mkPackageAttr = path: let
+        package = pkgs.callPackage path {inherit nvSources;};
+      in {
+        name = package.pname;
+        value = package;
+      };
+    in
+      [
+        ./wivrn-solarxr.nix
+      ]
+      |> map mkPackageAttr
+      |> builtins.listToAttrs;
   };
 }
