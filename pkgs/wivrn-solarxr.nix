5ae31c01b107325b5e544e45eefa42769e1888c7
Different <hello@different-name.dev>
2025-05-15 02:36:57 +1000

    formatting

--- pkgs/wivrn-solarxr.nix
+++ pkgs/wivrn-solarxr.nix
@@ -6,7 +6,7 @@
 }:
 # slimevr tracker support for wivrn
 # https://lvra.gitlab.io/docs/fossvr/wivrn/#nixos-setup
-(pkgs.wivrn.overrideAttrs (old: {
+pkgs.wivrn.overrideAttrs (old: {
   inherit (nvSources.wivrn) version src;
   pname = "wivrn-solarxr";
   cmakeFlags =
@@ -30,4 +30,4 @@
       ${nvSources.wivrn.src}/patches/apply.sh ${nvSources.wivrn.src}/patches/monado/*
     '';
   };
-}))
+})
