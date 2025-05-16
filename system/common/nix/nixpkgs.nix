aebfaa7e5892650fcb5e60a2de52a9112d858002
Different <hello@different-name.dev>
2025-05-16 20:32:48 +1000

    system/nixpkgs: patch slimevr to allow launching server and gui seperately

--- system/common/nix/nixpkgs.nix
+++ system/common/nix/nixpkgs.nix
@@ -2,6 +2,7 @@
   lib,
   config,
   inputs,
+  self,
   ...
 }: {
   imports = [
@@ -11,7 +12,25 @@
   options.nix-files.nix.nixpkgs.enable = lib.mkEnableOption "Nixpkgs config";
 
   config = lib.mkIf config.nix-files.nix.nixpkgs.enable {
-    nixpkgs.config.allowUnfree = true;
+    nixpkgs = {
+      config.allowUnfree = true;
+
+      overlays = [
+        (final: prev: {
+          slimevr = prev.slimevr.overrideAttrs (old: {
+            patches =
+              (old.patches or [])
+              ++ [
+                # https://nix.dev/guides/best-practices.html#reproducible-source-paths
+                (builtins.path {
+                  path = "${self}/patches/slimevr/launch-slimevr-server-seperately.patch";
+                  name = "slimevr-launch-slimevr-server-seperately";
+                })
+              ];
+          });
+        })
+      ];
+    };
 
     environment.sessionVariables.NIXPKGS_ALLOW_UNFREE = "1";
   };
