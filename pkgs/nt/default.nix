ac395d7b10284a5c8ab23724823cdabb294cc1c9
Different <hello@different-name.dev>
2025-05-16 12:43:54 +1000

    pkgs/nt: fish completions closes: #82

--- pkgs/nt/default.nix
+++ pkgs/nt/default.nix
@@ -1,9 +1,29 @@
-{pkgs, ...}:
-pkgs.writeShellApplication {
-  name = "nt";
-  runtimeInputs = with pkgs; [
-    nh
-    nvfetcher
-  ];
-  text = builtins.readFile ./nt.sh;
-}
+{pkgs, ...}: let
+  wrapped = pkgs.writeShellApplication {
+    name = "nt";
+    runtimeInputs = with pkgs; [
+      nh
+      nvfetcher
+    ];
+    text = builtins.readFile ./nt.sh;
+  };
+
+  fishCompletions = pkgs.runCommand "nt-fish-completions" {} ''
+    # fish completions
+    # zsh and bash completions are excluded because I don't use them to test
+    if [ -f "${pkgs.nh}/share/fish/vendor_completions.d/nh.fish" ]; then
+      mkdir -p $out/share/fish/vendor_completions.d
+
+      sed 's/complete -c nh/complete -c nt/g' \
+        ${pkgs.nh}/share/fish/vendor_completions.d/nh.fish \
+        > $out/share/fish/vendor_completions.d/nt.fish
+    fi
+  '';
+in
+  pkgs.symlinkJoin {
+    inherit (wrapped) name;
+    paths = [
+      wrapped
+      fishCompletions
+    ];
+  }
