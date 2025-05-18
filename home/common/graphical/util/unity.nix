d3dee573e158fb2350c35ba6a022352d7607c749
Different <hello@different-name.dev>
2025-05-18 15:01:12 +1000

    home/unity: + alcom

--- home/common/graphical/util/unity.nix
+++ home/common/graphical/util/unity.nix
@@ -8,22 +8,56 @@
 
   config = lib.mkIf config.nix-files.graphical.util.unity.enable {
     home = {
+      # generate scripts to launch unity editors directly, skipping the unity hub
+      # can be used as unity editor in alcom
+      file = let
+        unityEditors = [
+          "2022.3.22f1"
+        ];
+        editorDir = "Documents/Unity/.hub/Editor";
+      in
+        unityEditors
+        |> map (version: "${editorDir}/${version}/Editor")
+        |> map (path: {
+          name = "${path}/unity-run";
+          value = {
+            executable = true;
+            text = ''
+              #! /bin/sh
+              exec ${lib.getExe pkgs.unityhub.fhsEnv} ${path}/Unity "$@"
+            '';
+          };
+        })
+        |> lib.listToAttrs;
+
       packages = with pkgs; [
+        # https://github.com/tauri-apps/tauri/issues/9394
+        (symlinkJoin {
+          name = "alcom";
+          paths = [alcom];
+          buildInputs = [makeWrapper];
+          postBuild = ''
+            wrapProgram $out/bin/ALCOM \
+              --set WEBKIT_DISABLE_DMABUF_RENDERER 1
+          '';
+        })
         unityhub
-        vrc-get
       ];
 
       persistence."/persist${config.home.homeDirectory}" = {
         directories = [
+          # alcom
+          ".local/share/ALCOM"
+          ".local/share/VRChatCreatorCompanion"
+          ".local/share/com.anatawa12.vrc-get-gui"
+          ".cache/ALCOM"
+
           # unityhub
           ".config/unityhub"
           ".config/Unity"
           ".config/unity3d" # seems to also be for unity games
           ".local/share/unity3d"
           ".cache/unity3d"
-
-          # vrc-get
-          ".local/share/VRChatCreatorCompanion"
         ];
       };
     };
