24f6721a738269f62dd41da07696bc3bc0280b7a
Different <hello@different-name.dev>
2025-07-07 17:18:13 +1000

    revert(dyad/home): remove user & host options

--- dyad/home/default.nix
+++ dyad/home/default.nix
@@ -1,16 +1,6 @@
 { lib, ... }:
 {
   options.dyad = {
-    user = lib.mkOption {
-      description = "Which home-manager user configuration to use";
-      type = lib.types.str;
-    };
-
-    host = lib.mkOption {
-      description = "Which home-manager <user>@host configuration to use";
-      type = lib.types.str;
-    };
-
     flake = lib.mkOption {
       description = "Path to flake";
       type = lib.types.path;
