b566a4a415aa38abaa8dc3dc1c95952a110cfe82
Different <hello@different-name.dev>
2025-07-08 15:31:44 +1000

    feat(modules/home): impermanence-wrapper

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -2,6 +2,7 @@
   # keep-sorted start
   blender = ./blender;
   disblockOrigin = ./disblock-origin;
+  impermanenceWrapper = ./impermanence-wrapper;
   xdgDesktopPortalHyprland = ./xdg-desktop-portal-hyprland;
   # keep-sorted end
 }
