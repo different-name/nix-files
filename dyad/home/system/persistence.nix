31d2cc953fdf23bc06ffd1856fd72b79e8a6d5c7
Different <hello@different-name.dev>
2025-07-08 11:23:44 +1000

    feat(dyad/home): add persistence.installPkgsWithPersistence option

--- dyad/home/system/persistence.nix
+++ dyad/home/system/persistence.nix
@@ -2,9 +2,27 @@
   lib,
   config,
   inputs,
+  pkgs,
   ...
 }:
 let
+  inherit (lib) types;
+
+  # accept generic types anything so that upstream handles typing instead
+  # this is less maintenance in the event typing changes
+  persistOptions = {
+    dirs = lib.mkOption {
+      type = lib.types.listOf lib.types.anything;
+      default = [ ];
+      description = "directories to pass to persistence config";
+    };
+    files = lib.mkOption {
+      type = lib.types.listOf lib.types.anything;
+      default = [ ];
+      description = "files to pass to persistence config";
+    };
+  };
+
   cfg = config.dyad.system.persistence;
 in
 {
@@ -14,26 +32,39 @@ in
 
   options.dyad.system.persistence = {
     enable = lib.mkEnableOption "persistence config";
+    inherit (persistOptions) dirs files;
 
-    # we accept a list of anything so that impermanence will handle the typing instead
-    # this is less maintenance in the event impermemance changes typing
-    dirs = lib.mkOption {
-      type = lib.types.listOf lib.types.anything;
-      default = [ ];
-      description = "directories to pass to persistence config";
-    };
-
-    files = lib.mkOption {
-      type = lib.types.listOf lib.types.anything;
-      default = [ ];
-      description = "files to pass to persistence config";
+    installPkgsWithPersistence = lib.mkOption {
+      type = types.attrsOf (
+        types.submodule (
+          { name, ... }:
+          {
+            options = {
+              inherit (persistOptions) dirs files;
+              package = lib.mkOption {
+                type = types.package;
+                default = pkgs.${name};
+                description = "Package to install";
+              };
+            };
+          }
+        )
+      );
+      default = { };
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    home.persistence."/persist" = {
-      inherit (cfg) files;
-      directories = cfg.dirs;
+  config =
+    let
+      packageList = lib.attrValues cfg.installPkgsWithPersistence;
+
+      directories = cfg.dirs ++ (lib.flatten <| map (value: value.dirs) packageList);
+      files = cfg.files ++ (lib.flatten <| map (value: value.files) packageList);
+    in
+    lib.mkIf cfg.enable {
+      home = {
+        packages = map (value: value.package) packageList;
+        persistence."/persist" = { inherit directories files; };
+      };
     };
-  };
 }
