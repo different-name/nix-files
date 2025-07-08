6feaeb73f933dbc4bb2337effa7b5bced48dc7d4
Different <hello@different-name.dev>
2025-07-08 12:48:48 +1000

    revert(dyad/home): remove btop disks workaround

--- dyad/home/terminal/btop.nix
+++ dyad/home/terminal/btop.nix
@@ -1,7 +1,6 @@
 {
   lib,
   config,
-  osConfig,
   ...
 }:
 {
@@ -15,27 +14,6 @@
         proc_gradient = false;
         proc_mem_bytes = false;
         show_swap = false;
-
-        # all system level persisted directories need to be exlcuded through the disk filter
-        # otherwise they will show up as a physical disk in btop
-        disks_filter =
-          let
-            excludeDirectories =
-              # retrieve all system level persited directories
-              (lib.flatten (
-                map
-                  # map each persistence config to a list of persisted directories
-                  (persistenceConfig: map (dir: dir.dirPath) persistenceConfig.directories)
-                  # data from system's persistence configs
-                  (builtins.attrValues osConfig.environment.persistence)
-              ))
-              # manual additions
-              ++ [
-                "/boot"
-                "${config.home.homeDirectory}/.steam"
-              ];
-          in
-          lib.mkIf config.dyad.system.persistence.enable "exclude=${lib.concatStringsSep " " excludeDirectories}";
       };
     };
   };
