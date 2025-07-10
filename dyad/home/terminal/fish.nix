4ea61e0761e0cd5aa8dc9cd325a85c8f66dfaf34
Different <hello@different-name.dev>
2025-07-10 19:26:51 +1000

    chore(dyad/home): update yazi alias

--- dyad/home/terminal/fish.nix
+++ dyad/home/terminal/fish.nix
@@ -15,18 +15,6 @@ in
         set -U fish_color_cwd "${accentColor}"
         set -U fish_color_user "${accentColor}"
       '';
-
-      functions = {
-        # wrapper for yazi https://yazi-rs.github.io/docs/quick-start/#shell-wrapper
-        yy = ''
-          set tmp (mktemp -t "yazi-cwd.XXXXXX")
-          yazi $argv --cwd-file="$tmp"
-          if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-            cd -- "$cwd"
-          end
-          rm -f -- "$tmp"
-        '';
-      };
     };
 
     home.perpetual.default.dirs = [
--- dyad/home/terminal/yazi.nix
+++ dyad/home/terminal/yazi.nix
@@ -1,9 +1,26 @@
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 {
   options.dyad.terminal.yazi.enable = lib.mkEnableOption "yazi config";
 
   config = lib.mkIf config.dyad.terminal.yazi.enable {
     programs.yazi.enable = true;
+
+    # wrapper for yazi https://yazi-rs.github.io/docs/quick-start/#shell-wrapper
+    programs.fish.functions = {
+      yazi = ''
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        ${lib.getExe pkgs.yazi} $argv --cwd-file="$tmp"
+        if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+            builtin cd -- "$cwd"
+        end
+        rm -f -- "$tmp"
+      '';
+    };
 
     home.perpetual.default.dirs = [
       "$stateHome/yazi"
