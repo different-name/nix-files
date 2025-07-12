f80451e57e23c9b4dbd1bf6600ba4196b0f46c76
Different <hello@different-name.dev>
2025-07-13 08:19:25 +1000

    refactor(dyad): move fish preferences to home

--- dyad/home/terminal/fish.nix
+++ dyad/home/terminal/fish.nix
@@ -1,9 +1,33 @@
 { lib, config, ... }:
+let
+  catppuccinPalette = lib.importJSON (config.catppuccin.sources.palette + /palette.json);
+  themeColors = catppuccinPalette.${config.catppuccin.flavor}.colors;
+  accentColor = themeColors."mauve".hex;
+in
 {
   options.dyad.terminal.fish.enable = lib.mkEnableOption "fish config";
 
   config = lib.mkIf config.dyad.terminal.fish.enable {
-    programs.fish.enable = true;
+    programs.fish = {
+      enable = true;
+
+      interactiveShellInit = ''
+        # launch tmux
+        if status is-interactive
+        and not set -q TMUX
+          exec ${lib.getExe config.programs.tmux.package}
+        end
+
+        # disable greeting
+        set fish_greeting
+
+        set -U fish_color_cwd "${accentColor}"
+        set -U fish_color_user "${accentColor}"
+      '';
+
+      # tmux workaround https://github.com/fish-shell/fish-shell/issues/11538
+      binds."ctrl-h".command = "backward-kill-word";
+    };
 
     home.perpetual.default.dirs = [
       # keep-sorted start
--- dyad/nixos/programs/fish.nix
+++ dyad/nixos/programs/fish.nix
@@ -4,44 +4,19 @@
   pkgs,
   ...
 }:
-let
-  catppuccinPalette = lib.importJSON (config.catppuccin.sources.palette + /palette.json);
-  themeColors = catppuccinPalette.${config.catppuccin.flavor}.colors;
-  accentColor = themeColors."mauve".hex;
-in
 {
-  options.dyad.programs.fish.enable = lib.mkEnableOption "fish config";
+  options.dyad.programs.fish.enable = lib.mkEnableOption "fish as default shell";
 
   config = lib.mkIf config.dyad.programs.fish.enable {
-    programs.fish = {
-      enable = true;
-
-      interactiveShellInit = ''
-        set -U fish_color_cwd "${accentColor}"
-        set -U fish_color_user "${accentColor}"
-
-        # launch tmux
-        if status is-interactive
-        and not set -q TMUX
-            exec ${lib.getExe config.programs.tmux.package}
-        end
-
-        set fish_greeting # disable greeting
-
-        # https://github.com/fish-shell/fish-shell/issues/11538
-        bind ctrl-h backward-kill-word
-      '';
-    };
+    programs.fish.enable = true;
 
     # use fish as shell https://nixos.wiki/wiki/Fish
-    programs.bash = {
-      interactiveShellInit = ''
-        if [[ $(${lib.getExe' pkgs.procps "ps"} --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-        then
-          shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-          exec ${lib.getExe pkgs.fish} $LOGIN_OPTION
-        fi
-      '';
-    };
+    programs.bash.interactiveShellInit = ''
+      if [[ $(${lib.getExe' pkgs.procps "ps"} --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        exec ${lib.getExe pkgs.fish} $LOGIN_OPTION
+      fi
+    '';
   };
 }
