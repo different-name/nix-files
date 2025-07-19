ccd3c15cf5f73632d7775d5628b59eb27a81f9e0
diffy <hello@different-name.dev>
2025-07-19 14:02:41 +1000

    feat(dyad/home): goxlr toggle mute keybind

--- dyad/home/media/goxlr-utility/default.nix
+++ dyad/home/media/goxlr-utility/default.nix
@@ -13,18 +13,31 @@
       source = ./procaster.goxlrMicProfile;
     };
 
-    xdg.autostart.entries = [
-      (
-        (pkgs.makeDesktopItem {
-          name = "goxlr-daemon";
-          destination = "/";
-          desktopName = "GoXLR Daemon";
-          noDisplay = true;
-          exec = lib.getExe' osConfig.services.goxlr-utility.package "goxlr-daemon";
-        })
-        + /goxlr-daemon.desktop
-      )
-    ];
+    xdg.autostart.entries = lib.singleton (
+      (pkgs.makeDesktopItem {
+        name = "goxlr-daemon";
+        destination = "/";
+        desktopName = "GoXLR Daemon";
+        noDisplay = true;
+        exec = lib.getExe' osConfig.services.goxlr-utility.package "goxlr-daemon";
+      })
+      + /goxlr-daemon.desktop
+    );
+
+    wayland.windowManager.hyprland.settings.bind =
+      let
+        toggleMute = pkgs.writeShellApplication {
+          name = "goxlr-utility-tg-mute";
+          runtimeInputs = with pkgs; [
+            jq
+            goxlr-utility
+          ];
+          text = builtins.readFile ./toggle-mute.sh;
+        };
+      in
+      [
+        "$mod, Z, exec, ${lib.getExe toggleMute}"
+      ];
 
     home.perpetual.default.dirs = [
       # keep-sorted start
---
+++ dyad/home/media/goxlr-utility/toggle-mute.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+state=$(goxlr-client --status-json | jq -r '.mixers | to_entries[0].value.cough_button.state');
+
+if [ "$state" = "Unmuted" ]; then
+  goxlr-client cough-button mute-state muted-to-x;
+elif [ "$state" = "MutedToX" ]; then
+  goxlr-client cough-button mute-state unmuted;
+fi
