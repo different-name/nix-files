1be384a43f15f99c1ea5feaaa5b4b228f00d8cba
diffy <hello@different-name.dev>
2025-07-22 10:03:14 +1000

    feat(users/diffy): add pico public keys to authorized keys

--- configurations/users/diffy/default.nix
+++ configurations/users/diffy/default.nix
@@ -14,7 +14,8 @@ self.lib.mkUser
         # keep-sorted start
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHNtFP6sku6bgMrh8fmmnuikTxObmbiRLFGQOIcm5+KD diffy@potassium"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIoe3VveHt2vXoHdkRbLE0Xx5il0T3v8PiWxFvdniSLg diffy@sodium"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILnaWNZ2Q4sAkqK1KFbNfNb84l7uWVwCE7HnIHJzD8r1" # phone
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJgwHkHZhWjbZdto1j13LZ2KU8CljqLsTkXYKHK4Qurc diffy@pico"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILnaWNZ2Q4sAkqK1KFbNfNb84l7uWVwCE7HnIHJzD8r1 diffy@s23u"
         # keep-sorted end
       ];
 
