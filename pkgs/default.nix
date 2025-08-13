a9e8dfc0211b73f9c622f8e27515ca213060abd2
diffy <hello@different-name.dev>
2025-08-13 18:44:16 +1000

    feat(pkgs): allow access to self's packages within pkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -19,6 +19,8 @@
       packages =
         builtins.readDir ./.
         |> lib.filterAttrs (_: value: value == "directory")
-        |> lib.mapAttrs (name: _: pkgs.callPackage ./${name} { inherit (self') sources; });
+        |> lib.mapAttrs (
+          name: _: pkgs.callPackage ./${name} ({ inherit (self') sources; } // self'.packages)
+        );
     };
 }
