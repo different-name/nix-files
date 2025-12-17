372e68826743727ce136e277d0bb3fde3e5faf46
diffy <hello@different-name.dev>
2025-12-17 16:38:11 +1000

    fix(dyad/home/niri): enable niri through niri option

--- dyad/home/desktop/niri/default.nix
+++ dyad/home/desktop/niri/default.nix
@@ -6,7 +6,7 @@
 {
   options.dyad.desktop.niri.enable = lib.mkEnableOption "niri config";
 
-  config = lib.mkIf config.dyad.desktop.hyprland.enable {
+  config = lib.mkIf config.dyad.desktop.niri.enable {
     xdg.configFile."niri/config.kdl" = {
       source = ./config.kdl;
       force = true;
