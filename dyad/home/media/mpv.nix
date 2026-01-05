7b20bb4b2da1f557fcdbeb9379b5a642c6aa49cf
diffy <hello@different-name.dev>
2026-01-05 11:59:05 +1000

    fix(dyad/home/mpv): switch audio to pulseaudio for discord stream support

--- dyad/home/media/mpv.nix
+++ dyad/home/media/mpv.nix
@@ -3,7 +3,13 @@
   options.dyad.media.mpv.enable = lib.mkEnableOption "mpv config";
 
   config = lib.mkIf config.dyad.media.mpv.enable {
-    programs.mpv.enable = true;
+    programs.mpv = {
+      enable = true;
+      config = {
+        # fix for discord stream audio capture
+        ao = "pulse";
+      };
+    };
 
     home.perpetual.default.dirs = [
       "$cacheHome/mpv"
