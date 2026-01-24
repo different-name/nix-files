41bf7c2126ef8edaa49bd227bc6e009fe90f4de5
diffy <hello@different-name.dev>
2026-01-24 18:59:09 +1000

    chore: switch from flox to nixos-cuda

--- dyad/nixos/nix/substituters.nix
+++ dyad/nixos/nix/substituters.nix
@@ -10,7 +10,7 @@
 
         "https://hyprland.cachix.org"
         "https://nix-community.cachix.org"
-        "https://cache.flox.dev"
+        "https://cache.nixos-cuda.org"
       ];
 
       trusted-public-keys = [
@@ -18,7 +18,7 @@
 
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
+        "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
       ];
     };
   };
