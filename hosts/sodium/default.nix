7bd424072eb0e7135da543038caf3a965f48f1a1
diffy <hello@different-name.dev>
2026-01-24 19:01:55 +1000

    feat(hosts/sodium): setup postgres

--- hosts/sodium/default.nix
+++ hosts/sodium/default.nix
@@ -1,6 +1,7 @@
 {
   inputs,
   self,
+  pkgs,
   ...
 }:
 {
@@ -57,4 +58,15 @@
   hardware.brillo.enable = true; # backlight control
   # hardware.keyboard.qmk.enable = true; TODO uncomment when fixed: https://github.com/nixos/nixpkgs/issues/472891
   services.goxlr-utility.enable = true;
+
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql_17;
+
+    authentication = pkgs.lib.mkOverride 10 ''
+      #type database DBuser origin-address auth-method
+      local all      all     trust
+      host  all      all     127.0.0.1/32   trust
+    '';
+  };
 }
